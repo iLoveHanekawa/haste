d16951d5c9901f83d0be0aedabb8aab1bfa7474b
Arjun Tanwar <arjunthakur900@gmail.com>
2022-12-30 21:16:56 +0530

    fixes

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -4,8 +4,8 @@ function Hero() {
   return (
     <div className = 'mt-20 mb-5 flex flex-col items-center justify-center'>
         <div className = 'font-pacifico text-blue-400 text-7xl md:text-8xl'>Haste</div>
-        <div className = 'text-blue-300 text-xxs mt-6 md:text-xs'>Create databases faster using AI.</div>
-        <div className = 'text-blue-300 text-xxs md:text-xs'> Start by editing the Category key.</div>
+        <div className = 'text-blue-300 text-xs mt-6'>Create databases faster using AI.</div>
+        <div className = 'text-blue-300 text-xs'> Start by editing the Category key.</div>
     </div>
   )
 }   
