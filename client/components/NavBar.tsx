c10575f4f87eb4718091930d08261c38bbbecd6c
Arjun Tanwar <arjunthakur900@gmail.com>
2022-12-31 00:57:48 +0530

    fix

--- client/components/NavBar.tsx
+++ client/components/NavBar.tsx
@@ -6,11 +6,11 @@ function NavBar() {
   return (
     <nav className = 'w-screen text-white bg-cyan-400 flex justify-between items-center px-2 py-5'>
         <div className = ' flex items-end gap-1 text-sm md:text-md md:font-bold md:tracking-wider lg:text-lg lg:gap-2'>
-            <GiSpeedometer className = 'text-2xl md:text-3xl lg:text-4xl' />
+            <GiSpeedometer className = 'text-3xl md:text-4xl lg:text-5xl' />
             <div>Haste</div>
         </div>
         <a target={'_blank'} rel = 'noreferrer' className = 'cursor-pointer' href='https://github.com/iLoveHanekawa/haste'>
-            <AiFillGithub className = 'text-2xl md:text-3xl lg:text-4xl' />
+            <AiFillGithub className = 'text-3xl md:text-4xl lg:text-5xl' />
         </a>
     </nav>
   )
